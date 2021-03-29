630184cc8bc3a64138ba9d8d40eabb8b02ccb1cc
LIA LIN <1586250561@qq.com>
2021-03-29 22:15:21 +0800

    py test for if

--- pythontest/pytest1.py
+++ pythontest/pytest1.py
@@ -1,1 +1,13 @@
-print('helloworld')
+#print('helloworld')
+#input & output
+#print('name?')
+#name = input()
+#print(name,',你真是个小可爱')
+
+#if
+a = input()
+num = int(a)
+if num>= 100:
+    print('yes')
+else:
+        print(a)
